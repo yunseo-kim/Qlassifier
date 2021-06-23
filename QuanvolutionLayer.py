dbf266295b4a8d71761948b170efbc26f53a34cf
Hojun Lee <quantum0430@gmail.com>
2021-06-23 17:30:30 +0900

    PQC class

---
+++ QuanvolutionLayer.py
@@ -0,0 +1,11 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+class QuanvolutionLayer(torch.nn.Module):
+    def __init__(self):
+        super(QuanvolutionLayer, self).__init__()
+
+    def forward(self,x):
+        return 0
+    def backward()
